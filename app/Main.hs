4b93a11673c2fdf91864f4cda388f6650c2bf1d1
Marek Dudek <marek.dudek@gmail.com>
2018-06-14 12:46:40 +0200

    demo of CPS-Pyth

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,15 @@
 module Main where
 
 import Lib
+import Pythagorean
 
 main :: IO ()
-main = someFunc
+main = demoPyth
+
+
+demoPyth = 
+  do
+    let a = 3
+        b = 4
+    putStr $ "Pythagorean " ++ show a ++ " and " ++ show b ++ " is "
+    pyth' a b print
